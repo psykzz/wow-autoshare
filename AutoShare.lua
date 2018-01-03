bbcdbeb6432651281e8a94934123b8c6c70aa224
Matt Smith <psykzz@users.noreply.github.com>
2018-01-03 22:32:39 +0000

    Update AutoShare.lua

--- AutoShare.lua
+++ AutoShare.lua
@@ -1,4 +1,3 @@
-local AS_Version = "1.0.0";
 local AS_Name = "Auto Quest Sharing";
 local AS_BLUE = "|c000099ff";
 local AS_YELLOW = "|cffffff55";
@@ -6,7 +5,6 @@ local AS_END_COLOR = "|r";
 local AS_Title = AS_BLUE .. AS_Name .. ":" .. AS_END_COLOR .. " ";
 
 local EVENTS = {};
-local MAX_LEVEL = 100;
 
 -- Event ADDON_LOADED
 EVENTS.ADDON_LOADED = "ADDON_LOADED";
@@ -21,12 +19,12 @@ function AS_OnLoad(self)
 		self:RegisterEvent(strupper(v));
 	end
 
-	AS_SendMessage(AS_BLUE .. AS_Name .. " v" .. AS_Version .. " by PsyKzz." .. AS_END_COLOR);
+	AS_SendMessage(AS_BLUE .. AS_Name .. " by PsyKzz." .. AS_END_COLOR);
 end
 
 function AS_OnEvent(self, event, ...)
 	if event == EVENTS.ADDON_LOADED and ... == "AutoShare" then
-		AS_SendMessage(AS_BLUE .. AS_Name .. " v" .. AS_Version .. " loaded." .. AS_END_COLOR);
+		AS_SendMessage(AS_BLUE .. AS_Name .. " loaded." .. AS_END_COLOR);
 	elseif event == EVENTS.QUEST_ACCEPTED then
 		AS_HandleQuestAccepted(...);
 	end
@@ -34,10 +32,7 @@ end
 
 function AS_HandleQuestAccepted(questIndex)
 	if GetNumGroupMembers() >= 1 then
-		if AS_Options.announce then
-			SendChatMessage("[" .. AS_Name .. "] Quest accepted: " .. GetQuestLink(questIndex), "PARTY");
-		end
-
+		
 		SelectQuestLogEntry(questIndex);
 
 		if GetQuestLogPushable() then
